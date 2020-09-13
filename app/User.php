21d29a20227ce18e64bc9d68849715427b955734
moinultm <moinultm@gmail.com>
2020-09-13 21:50:42 +0600

    updates

--- app/User.php
+++ app/User.php
@@ -36,4 +36,59 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function hasRole($role) {
+        $roles = $this->roles()->pluck('name')->toArray();
+        return in_array($role, $roles);
+    }
+
+
+    public function roles() {
+        return $this->belongsToMany('App\Role');
+    }
+
+    public function can($permission, $arguments = []) {
+        foreach($this->roles as $role) {
+            if($role->can($permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Get full name attribute
+     * */
+
+    public function getNameAttribute(){
+        return $this->first_name.' '.$this->last_name;
+    }
+
+    public function setEmailAttribute($value) {
+        $this->attributes['email'] = self::normalizeEmail($value);
+    }
+
+    public function setPasswordAttribute($password) {
+        if(!is_null($password))
+            $this->attributes['password'] = bcrypt($password);
+    }
+
+    public static function normalizeEmail($value) {
+        return trim(strtolower($value));
+    }
+
+
+    public function getPermissionsAttribute () {
+        $permissions = [];
+        foreach ($this->roles as $role) {
+            $rolePermissions = [];
+            foreach ($role->permissions as $permission) {
+                $rolePermissions[] = $permission->type.'.'.$permission->name;
+            }
+            $permissions = array_merge($permissions, $rolePermissions);
+        }
+        return $permissions;
+    }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -10,10 +10,56 @@ ACL
 
 @section('content')
 
-    @foreach($users as $user)
-        <tr>
-            <td class="text-center">{{$loop->iteration}}</td>
-            <td class="text-center">{{$user->name}}</td>
-            <td class="text-center">{{$user->email}}</td>
-    @endforeach
+       <!-- Page Heading -->
+             <p class="mb-4">  </p>
+
+            <!-- DataTales Example -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                            <tr>
+
+                                <th>Sl</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email Address</th>
+                                <th>Roles</th>
+                                <th>Status</th>
+                            </tr>
+                            </thead>
+                            <tfoot>
+                            <tr>
+                                <th>Sl</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email Address</th>
+                                <th>Roles</th>
+                                <th>Status</th>
+                            </tr>
+                            </tfoot>
+                            <tbody>
+
+                            @foreach($users as $user)
+                                <tr>
+                                    <td class="text-center">{{$loop->iteration}}</td>
+                                    <td class="text-center">{{$user->first_name}}</td>
+                                    <td class="text-center">{{$user->last_name}}</td>
+                                    <td class="text-center">{{$user->email}}</td>
+                                    @if(auth()->user()->can('user.manage'))
+
+                                    @endif
+                                </tr>
+                            @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
 @endsection
