c5a5a03d8020447fcf21ae8634b19ae0028a2de2
moinultm <moinultm@gmail.com>
2020-09-13 22:04:40 +0600

    auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,38 +4,96 @@
 @include('partials.htmlheader')
 
 <body id="page-top">
-  <!-- Page Wrapper -->
+
+<!--App-->
+<div id="app">
+<!-- Page Wrapper -->
 <div id="wrapper">
- @include('partials.sidebar')
-  <!-- Content Wrapper -->
- <div id="content-wrapper" class="d-flex flex-column">
-   <!-- Main Content -->
-   <div id="content">
-   @include('partials.mainheader')
-   <!-- Begin Page Content -->
-     <div class="container-fluid">
-     @include('partials.contentheader')
-     @yield('content')
+@include('partials.sidebar')
+<!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+        <!-- Main Content -->
+        <div id="content">
+        @include('partials.mainheader')
+        <!-- Begin Page Content -->
+            <div class="container-fluid">
+                @include('partials.contentheader')
 
-   </div>
-   <!--  End of Page Content -->
-  </div>
-  <!-- End of Main Content -->
-   @include('partials.footer')
-  </div>
-  <!--End Content Wrapper -->
+                    @yield('content')
+
+            </div>
+            <!--  End of Page Content -->
+        </div>
+        <!-- End of Main Content -->
+        @include('partials.footer')
+    </div>
+    <!--End Content Wrapper -->
 </div>
-  <!--End Page Wrapper -->
+<!--End Page Wrapper -->
 
 
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+    </div>
+<!--End App  -->
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
-  </a>
+</a>
 
-  @section('js')
+    @section('js')
     @include('partials.scripts')
-  @show
-  </body>
-  
-  </html>
+    @show
+    </body>
+
+    </html>
