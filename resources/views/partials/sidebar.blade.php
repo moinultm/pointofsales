e623976ceff29750d5bef36f5fe382f9405b4fe7
moinultm <moinultm@gmail.com>
2020-10-12 14:37:51 +0600

    settings okay

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -76,14 +76,21 @@
         </a>
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
+
+                <a class="collapse-item" href="404.html">Articles</a>
+                <a class="collapse-item" href="404.html">Photo Gallery </a>
+                <a class="collapse-item" href="404.html">Advertisements</a>
+                <a class="collapse-item" href="404.html">Category</a>
+                <a class="collapse-item" href="404.html">Area</a>
+
+                <!--h6 class="collapse-header">Login Screens:</h6>
                 <a class="collapse-item" href="login.html">Login</a>
                 <a class="collapse-item" href="register.html">Register</a>
                 <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
                 <div class="collapse-divider"></div>
                 <h6 class="collapse-header">Other Pages:</h6>
                 <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item" href="blank.html">Blank Page</a>
+                <a class="collapse-item" href="blank.html">Blank Page</a-->
             </div>
         </div>
     </li>
